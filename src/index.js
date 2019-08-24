fb82c7b1b1159868e7788a4c4166ad9da74e77cb
Rodrigo Mesquita <rodrigoriome@gmail.com>
2019-08-23 21:09:55 -0300

    fix database config and env settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv").config();
 const app = require("./app");
 
-if (dotenv.error) throw dotenv.error;
+if (process.env.NODE_ENV !== "production" && dotenv.error) throw dotenv.error;
 
 app.listen(process.env.PORT || 3000);
