3e84c9c042eff14a77deb8c3c51775c4d32e020f
Rodrigo Mesquita <rodrigoriome@gmail.com>
2019-08-23 21:48:04 -0300

    remove mime-type validation

--- src/app/config/multer.js
+++ src/app/config/multer.js
@@ -3,14 +3,10 @@ const multerConfig = {
     fileSize: 2 * 1024 * 1024
   },
   fileFilter: (req, file, cb) => {
-    const allowedMimes = ["text/csv", "application/csv", "application/octet-stream"];
     const allowedExtensions = ["csv"];
-    const fileMime = file.mimetype;
     const fileExtension = file.originalname.split(".").reverse()[0];
 
-    if (!allowedMimes.includes(fileMime)) {
-      cb(new Error(`Invalid file mime type. Expected "text/csv". Received: "${file.mimetype}"`));
-    } else if (!allowedExtensions.includes(fileExtension)) {
+    if (!allowedExtensions.includes(fileExtension)) {
       cb(new Error(`Invalid file extension. Expected "csv". Received: "${fileExtension}"`));
     } else {
       cb(null, true);
